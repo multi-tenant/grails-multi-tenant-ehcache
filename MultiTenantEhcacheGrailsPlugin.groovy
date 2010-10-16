c923939d583cf6ab3b72a7f88b762693e06aed38
sryan <scottryan@sryans-macbook-pro-17.local>
2010-10-16 10:44:56 -0600

    update to change plugin name

--- MultiTenantEhcacheGrailsPlugin.groovy
+++ MultiTenantEhcacheGrailsPlugin.groovy
@@ -5,13 +5,13 @@ class MultiTenantEhcacheGrailsPlugin
   // the version or versions of Grails the plugin is designed for
   def grailsVersion = "1.3.0 > *"
   // the other plugins this plugin depends on
-  def dependsOn = [grailsMultiTenantCore : "1.0.0-SNAPSHOT"]
+  def dependsOn = [multiTenantCore : "1.0.0-SNAPSHOT"]
   // resources that are excluded from plugin packaging
   def pluginExcludes = [
           "grails-app/views/error.gsp"
   ]
   def author = "Scott Ryan"
-  def authorEmail = "scryan@codehaus.com"
+  def authorEmail = "scryan@codehaus.org"
   def title = "Plugin to support EH Cache 2nd level support when running in multiple database mode"
   def description = '''This plugin provides a multi tenant implmentation of the EHCache provider to support one
                        database per client.  This plugin also provides the managment screens for viewing and
